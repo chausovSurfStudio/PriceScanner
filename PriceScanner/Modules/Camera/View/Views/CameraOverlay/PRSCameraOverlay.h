19389e016b6632afe6d6dac3f86e108858469a39
Александр Чаусов <traktoro_13@mail.ru>
2018-05-12 17:55:31 +0300

    #2013 добавлены состояния оверлея, цвет уголков меняется в зависимости от этого состояния

--- PriceScanner/Modules/Camera/View/Views/CameraOverlay/PRSCameraOverlay.h
+++ PriceScanner/Modules/Camera/View/Views/CameraOverlay/PRSCameraOverlay.h
@@ -9,6 +9,17 @@
 #import "PRSDesignableView.h"
 
 
+/** В данном перечислении объявлены состояния оверлея для экрана камеры */
+typedef NS_OPTIONS(NSUInteger, PRSCameraOverlayState) {
+    /** Состояние нективного оверлея, в котором он находится по-умолчанию */
+    PRSCameraOverlayStateWaiting = 0,
+    /** Состояние активного оверлея, "уголки" в нем обычно имеют другой цвет */
+    PRSCameraOverlayStateActive
+};
+
+
 @interface PRSCameraOverlay : PRSDesignableView
+
+@property (nonatomic, assign) PRSCameraOverlayState state;
 
 @end
--- PriceScanner/Modules/Camera/View/Views/CameraOverlay/PRSCameraOverlay.m
+++ PriceScanner/Modules/Camera/View/Views/CameraOverlay/PRSCameraOverlay.m
@@ -9,10 +9,13 @@
 #import "PRSCameraOverlay.h"
 
 
+static CGFloat const animationDuration = 0.3f;
+
 @interface PRSCameraOverlay()
 
-@property (strong, nonatomic) IBOutletCollection(UIView) NSArray *overlayBorderViews;
-
+@property (nonatomic, strong) IBOutletCollection(UIView) NSArray *overlayBorderViews;
+@property (nonatomic, strong) IBOutletCollection(UIView) NSArray *verticalCornerLines;
+@property (nonatomic, strong) IBOutletCollection(UIView) NSArray *horizontalCornerLines;
 
 @end
 
@@ -21,13 +24,53 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
+    _state = PRSCameraOverlayStateWaiting;
+    
     [self configureBorders];
+    [self configureCornerLines];
 }
+
+- (void)setState:(PRSCameraOverlayState)state {
+    _state = state;
+    UIColor *color = [self cornerColorForCurrentState];
+    [self changeCornerLinesColor:color animated:YES];
+}
+
+#pragma mark - Interface Methods
+
 
 #pragma mark - Configure
 - (void)configureBorders {
     for (UIView *borderView in self.overlayBorderViews) {
         borderView.backgroundColor = [[UIColor prsBlackTextColor] colorWithAlphaComponent:0.6f];
+    }
+}
+
+- (void)configureCornerLines {
+    UIColor *color = [self cornerColorForCurrentState];
+    [self changeCornerLinesColor:color animated:NO];
+}
+
+#pragma mark - Private Methods
+- (UIColor *)cornerColorForCurrentState {
+    return self.state == PRSCameraOverlayStateWaiting ? [UIColor whiteColor] : [UIColor prsMainThemeColor];
+}
+
+- (void)changeCornerLinesColor:(UIColor *)color animated:(BOOL)animated {
+    NSMutableArray<UIView *> *lines = [@[] mutableCopy];
+    [lines addObjectsFromArray:self.verticalCornerLines];
+    [lines addObjectsFromArray:self.horizontalCornerLines];
+    if (animated) {
+        [UIView animateWithDuration:animationDuration
+                         animations:^{
+                             for (UIView *line in lines) {
+                                 line.backgroundColor = color;
+                             }
+                         }];
+    } else {
+        for (UIView *line in lines) {
+            line.backgroundColor = color;
+        }
     }
 }
 
