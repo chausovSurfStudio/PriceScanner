39431ec5846d4cc4fe8728f07944d353b17606f6
Александр Чаусов <traktoro_13@mail.ru>
2018-05-15 14:12:10 +0300

    #2028 правка по ревью кода, добавление недостающих комментариев

--- PriceScanner/Modules/NativeCamera/View/Helpers/Scanner/PRSCharDetectResult.h
+++ PriceScanner/Modules/NativeCamera/View/Helpers/Scanner/PRSCharDetectResult.h
@@ -10,12 +10,14 @@
 #import <Vision/Vision.h>
 
 
+/** Вспомогательный объект, необходим для связывания между собой информации о распознанном в видеопотоке изображении и результате работы нейросети */
 @interface PRSCharDetectResult : NSObject
 
 @property (nonatomic, strong) VNRectangleObservation *charBox;
 @property (nonatomic, strong) NSString *prediction;
 @property (nonatomic, assign) CGFloat confidence;
 
+/** Возвращает YES, если все поля объекта заполнены и заполнены корректно, NO в противном случае */
 - (BOOL)isCorrectlyFilled;
 
 @end
